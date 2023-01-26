e1471e2d18c1e77150fb6bad6572f2e509f974e5
Amaru Korpimäki <amarukorpimaki@Amarus-MBP.home>
2023-01-26 21:03:02 +0200

    task 1 done

--- index.js
+++ index.js
@@ -25,10 +25,6 @@ let phonebook = [
   
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/persons', (request, response) => {
   response.json(phonebook)
 })
