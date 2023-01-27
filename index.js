d5d66c96f41023cee16ad8238c6002d75238cb1d
Amaru Korpimäki <amarukorpimaki@Amarus-MacBook-Pro.local>
2023-01-27 15:20:23 +0200

    task 2, task 3 done

--- index.js
+++ index.js
@@ -29,6 +29,26 @@ app.get('/api/persons', (request, response) => {
   response.json(phonebook)
 })
 
+app.get('/api/info', (resquest, response)=>{
+  const amountOfPeople = phonebook.length
+  const date = new Date()
+  response.send(`<h2>Phonebook has info of ${amountOfPeople} people </h2>
+              <h2>${date}</h2>`)
+  // response.send(`<h2>${date}</h2>`)
+})
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id)
+  const person = phonebook.find(person => person.id === id)
+  console.log(person)
+  if(person){
+    response.json(person)
+  }
+  else{
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
