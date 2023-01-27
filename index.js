895dbb421916e8b13869e29323f0c6dd72f47df2
Amaru Korpimäki <amarukorpimaki@Amarus-MacBook-Pro.local>
2023-01-27 17:09:11 +0200

    task 5 done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let phonebook = [
     { 
@@ -53,6 +56,30 @@ app.delete('/api/persons/:id', (request, response)=>{
   phonebook = phonebook.filter(person => person.id !== id)
   console.log(phonebook)
   response.status(204).end()
+})
+
+const generateId = () => {
+  const id = Math.floor((Math.random() * 100000) + 4);
+  return id
+}
+
+app.post('/api/persons', (request,response)=>{
+  const body = request.body
+  // console.log(body.content)
+
+  if(!body){
+    return response.status(400).json({
+    error: 'content missing'
+    })
+  }
+  const person = {
+    id: generateId(),
+    name: body.name, 
+    number: body.number
+  }
+
+  phonebook = phonebook.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001
